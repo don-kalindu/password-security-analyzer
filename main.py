5cdf7b8a2e336e109cef405dbc7a70e79cb2667e
Kalindu Manthila <manthilakalindu@gmail.com>
2026-09-03 15:45:35 +1000

    Initial project setup

---
+++ main.py
@@ -0,0 +1,8 @@
+print("=" * 50)
+print("     PASSWORD SECURITY ANALYZER")
+print("=" * 50)
+
+password = input("Enter the password to analyze : ")
+
+print("\nPassword Received.")
+print("Password length", len(password))
