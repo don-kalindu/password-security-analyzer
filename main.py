8c70d9b7ebb291d241dec90cc926141a9063defd
Kalindu Manthila <manthilakalindu@gmail.com>
2026-09-04 10:06:46 +1000

    Add scoring and strength levels

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ print("=" * 50)
 print("     PASSWORD SECURITY ANALYZER")
 print("=" * 50)
 
+score = 0
+
 password = input("Enter the password to analyze : ")
 
 print("\nPassword Received.")
@@ -23,3 +25,31 @@ if len(password) >= 12:
     print("Your password is Good")
 else:
     print("Your password is short")
+
+#scoring
+if has_uppercase:
+    score = score + 1
+
+if has_lowercase:
+    score = score + 1
+
+if has_numbers:
+    score = score +1
+
+if has_special:
+    score = score + 1
+
+if len(password) >= 12:
+    score = score + 1
+
+print("Security Score : ", score,"/5")
+
+#strength level
+if score == 5:
+    print("Password strength : STRONG")
+
+elif score == 3 or score == 4:
+    print("Password strength : MEDIUM")
+
+else:
+    print("Password strength : WEAK")
